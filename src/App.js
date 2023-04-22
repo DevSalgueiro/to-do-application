b3c1ef02b6a5e67bbb9abbe547fcb7a38576ab21
Salgueiro <luis.salgueiro@ensineme.com.br>
2023-04-22 18:02:00 -0300

    add js + css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { useState, useEffect } from "react";
+import { BsTrash, BsBookmarkCheck, BsBookmarkCheckFill } from "react-icons/bs";
+
+const API = "http://localhost:5000";
+
 function App() {
+
+  const [title, setTitle] = useState("");
+  const [time, setTime] = useState("");
+  const[todos, setTodos] = useState([]);
+  const[loading, setLoading] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    const todo = {
+      id: Math.random(),
+      title,
+      time,
+      done: false,
+    };
+
+    console.log(todo);
+
+    setTitle("");
+    setTime("");
+  };
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div className='todo-header'>
+        <h1>To Do</h1>
+      </div>
+
+      <div className='form-todo'>
+        <h2>Insira a sua tarefa:</h2>
+        <form onSubmit={handleSubmit}>
+
+          <div className='form-control'>
+            <label htmlFor="title">O que você vai fazer?</label>
+            <input type="text" name="title" placeholder="Título da tarefa" onChange={(e) => setTitle(e.target.value)} value={title || ""} required/>
+          </div>
+
+          <div className='form-control'>
+            <label htmlFor="time">Duração:</label>
+            <input type="text" name="time" placeholder="Tempo estimado (em horas)" onChange={(e) => setTime(e.target.value)} value={time || ""} required/>
+          </div>
+
+          <input type="submit" value="Criar tarefa" />
+        </form>
+      </div>
+
+      <div className='list-todo'>
+        <h2>Lista</h2>
+        {todos.length === 0 && <p>Não há tarefas!</p>}
+      </div>
+
     </div>
   );
 }
